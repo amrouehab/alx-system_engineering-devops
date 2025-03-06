c80d03336c049f0045a4c6a0f0c33d0b58d0d749
amrouehab <amrou.ehab@gmail.com>
2025-03-06 00:03:42 -0800

    0x1B-web_stack_debugging_4#

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,25 @@
 # This Puppet manifest increases the ULIMIT for Nginx to handle more concurrent connections
 # This fixes the high number of failed requests when benchmarking with ApacheBench
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
-  path    => '/usr/local/bin/:/bin/:/usr/bin/'
+file { '/etc/default/nginx':
+  ensure  => present,
+  content => "# /etc/default/nginx
+DAEMON_OPTS=\"\"
+ULIMIT=\"-n 4096\"
+",
+  notify => Service['nginx'],
+}
+
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
+
+# Also need to optimize nginx worker configuration for high concurrency
+file_line { 'worker_connections':
+  ensure => present,
+  path   => '/etc/nginx/nginx.conf',
+  line   => '    worker_connections 4096;',
+  match  => '^\s*worker_connections\s+[0-9]+;',
+  notify => Service['nginx'],
 }
--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,24 @@
 # This Puppet manifest increases the file limits for the holberton user
 # Fixes the "Too many open files" error when logging in as holberton
 
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "/holberton hard/c\holberton hard nofile 50000" /etc/security/limits.conf && sed -i "/holberton soft/c\holberton soft nofile 50000" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/:/usr/bin/'
+file_line { 'holberton_soft_limit':
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => 'holberton soft nofile 65536',
+  match  => '^holberton soft nofile',
+}
+
+file_line { 'holberton_hard_limit':
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => 'holberton hard nofile 65536',
+  match  => '^holberton hard nofile',
+}
+
+# Also configure PAM limits module to ensure limits.conf is applied
+file_line { 'enable_pam_limits':
+  ensure => present,
+  path   => '/etc/pam.d/common-session',
+  line   => 'session required pam_limits.so',
+  match  => '^session\s+required\s+pam_limits.so',
 }
