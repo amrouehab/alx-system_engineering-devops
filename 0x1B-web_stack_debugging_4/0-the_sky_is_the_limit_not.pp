94eff7d77621918c21a5eb5a82356238d46091b6
amrouehab <amrou.ehab@gmail.com>
2025-03-06 00:07:06 -0800

    0x1B-web_stack_debugging_4#

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,25 +1,13 @@
-# This Puppet manifest increases the ULIMIT for Nginx to handle more concurrent connections
-# This fixes the high number of failed requests when benchmarking with ApacheBench
+# Increases the amount of traffic an Nginx server can handle.
 
-file { '/etc/default/nginx':
-  ensure  => present,
-  content => "# /etc/default/nginx
-DAEMON_OPTS=\"\"
-ULIMIT=\"-n 4096\"
-",
-  notify => Service['nginx'],
-}
+# Increase the ULIMIT of the default file
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+} ->
 
-service { 'nginx':
-  ensure => running,
-  enable => true,
-}
-
-# Also need to optimize nginx worker configuration for high concurrency
-file_line { 'worker_connections':
-  ensure => present,
-  path   => '/etc/nginx/nginx.conf',
-  line   => '    worker_connections 4096;',
-  match  => '^\s*worker_connections\s+[0-9]+;',
-  notify => Service['nginx'],
+# Restart Nginx
+exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
