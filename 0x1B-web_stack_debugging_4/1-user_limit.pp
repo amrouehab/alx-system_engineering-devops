800621d6c41cd770548d8bc2460ff5414103be1c
amrouehab <amrou.ehab@gmail.com>
2025-03-05 23:04:24 -0800

    WebStackDebugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,7 +1,14 @@
-# This Puppet manifest increases the file limits for the holberton user
-# It fixes the "Too many open files" error when logging in and performing operations
+# Increase the open file limit for the holberton user
+file_line { 'holberton_soft_limit':
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => 'holberton soft nofile 65536',
+  match  => '^holberton soft nofile',
+}
 
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf && sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
-  path    => '/usr/local/bin/:/bin/:/usr/bin/'
+file_line { 'holberton_hard_limit':
+  ensure => present,
+  path   => '/etc/security/limits.conf',
+  line   => 'holberton hard nofile 65536',
+  match  => '^holberton hard nofile',
 }
