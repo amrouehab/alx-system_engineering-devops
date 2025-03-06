7647cfb0ac98a0e29ec12af63618fac5ffa0fa0b
amrouehab <amrou.ehab@gmail.com>
2025-03-06 00:15:33 -0800

    0x1B-web_stack_debugging_4#

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,24 +1,16 @@
-# This Puppet manifest increases the file limits for the holberton user
-# Fixes the "Too many open files" error when logging in as holberton
+# Enable the user holberton to login and open files without error.
 
-file_line { 'holberton_soft_limit':
-  ensure => present,
-  path   => '/etc/security/limits.conf',
-  line   => 'holberton soft nofile 65536',
-  match  => '^holberton soft nofile',
+# Increase hard file limit for Holberton user.
+file_line { 'increase-hard-file-limit-for-holberton-user':
+  path  => '/etc/security/limits.conf',
+  line  => 'holberton hard nofile 50000',
+  match => '^holberton hard nofile',
 }
 
-file_line { 'holberton_hard_limit':
-  ensure => present,
-  path   => '/etc/security/limits.conf',
-  line   => 'holberton hard nofile 65536',
-  match  => '^holberton hard nofile',
+# Increase soft file limit for Holberton user.
+file_line { 'increase-soft-file-limit-for-holberton-user':
+  path  => '/etc/security/limits.conf',
+  line  => 'holberton soft nofile 50000',
+  match => '^holberton soft nofile',
 }
 
-# Also configure PAM limits module to ensure limits.conf is applied
-file_line { 'enable_pam_limits':
-  ensure => present,
-  path   => '/etc/pam.d/common-session',
-  line   => 'session required pam_limits.so',
-  match  => '^session\s+required\s+pam_limits.so',
-}
