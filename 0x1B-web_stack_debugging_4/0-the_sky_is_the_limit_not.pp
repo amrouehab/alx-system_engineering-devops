d823a5a6356ef8af983df0d9108b6eb796e2c503
amrouehab <amrou.ehab@gmail.com>
2025-03-06 00:25:40 -0800

    0x1B-web_stack_debugging_4#

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,8 +1,12 @@
-# This Puppet manifest increases the ULIMIT for the Nginx service to handle more connections.
-# It fixes the "too many open files" error that causes failed requests under heavy load.
+# Optimizes Nginx to handle more connections by increasing ULIMIT and restarting the service.
 
-exec { 'fix-nginx-ulimit':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx && service nginx restart',
-  path    => ['/usr/local/bin', '/bin', '/usr/bin'],
+exec { 'increase-nginx-ulimit':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => ['/usr/local/bin', '/bin'],
+} ->
+
+exec { 'restart-nginx':
+  command => 'service nginx restart',
+  path    => ['/sbin', '/usr/sbin', '/bin', '/usr/bin'],
 }
 
